b06475795ca253108f18bf1292b416c0cc318440
samanthabertous <sam@samanthabertous.com>
2016-06-20 17:24:16 -0400

    set up basic jQuery functionality

---
+++ assets/js/todo.js
@@ -0,0 +1,22 @@
+//Check off Specific ToDos by clicking
+$("ul").on("click","li", function(){
+	$(this).toggleClass("completed")
+})
+
+//Delete Specific ToDos by clicking
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove()
+	})
+	event.stopPropagation()
+})
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo tet from input
+		var todoText = $(this).val()
+		$(this).val("")
+		//create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>")
+	}
+})
